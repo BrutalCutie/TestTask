454b3e27721fdc0bf302db7ba7fbc0427d6d85c3
tiken14 <tiken14@live.com>
2025-06-30 16:04:32 +0500

    Подключение приложений фильтров и drf. Добавил сериализатор и Вьюсет подукта

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    "mainapp"
+    "mainapp",
+
+    'rest_framework',
+    'django_filters',
+
 ]
 
 MIDDLEWARE = [
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from mainapp.models import Product
+from mainapp.serializers import ProductSerializer
 
-# Create your views here.
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = {
+        'price': ['gte', 'lte'],
+        'sale_price': ['gte', 'lte'],
+        'rating': ['gte'],
+        'reviews_count': ['gte'],
+    }
+    ordering_fields = ['name', 'price', 'rating', 'reviews_count']
