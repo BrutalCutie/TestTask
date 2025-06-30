c51764bb28a6f62ed4cddb80c0a7ad018c45beaf
tiken14 <tiken14@live.com>
2025-06-30 18:48:41 +0500

    Дописывание отображений и urls

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,6 +1,9 @@
+from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from mainapp.views import ProductViewSet
+
+from config import settings
+from mainapp.views import ProductViewSet, product_analytics
 
 
 router = DefaultRouter()
@@ -8,4 +11,9 @@ router.register(r'products', ProductViewSet, basename='product')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('analytics/', product_analytics, name='product_analytics'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
